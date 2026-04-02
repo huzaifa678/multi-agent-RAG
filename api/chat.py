cbce90679d3ec10bd2276a40fe518f6bc5da8e40
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-04-02 18:52:19 +0500

    added logging and exception handling on the endpoints

--- api/chat.py
+++ api/chat.py
@@ -1,12 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from langsmith import traceable
 from langsmith import traceable
 from services.chat_service import handle_chat
+from utils import logger
 
 router = APIRouter()
 
 @router.post("/chat")
 @traceable(name="chat_endpoint")
 async def chat(payload: dict):
-   
-    return await handle_chat(payload)
+    try:
+        logger.info(f"Incoming chat request: {payload}")
+
+        result = await handle_chat(payload)
+
+        logger.info("Chat request processed successfully")
+        return result
+
+    except HTTPException as e:
+        logger.warning(f"HTTPException occurred: {e.detail}")
+        raise e
+
+    except Exception as e:
+        logger.exception("Unexpected error in /chat endpoint")
+
+        raise HTTPException(
+            status_code=500,
+            detail="Internal Server Error"
+        )
--- api/upload.py
+++ api/upload.py
@@ -1,11 +1,29 @@
-from fastapi import APIRouter, File , UploadFile
+from fastapi import APIRouter, File, HTTPException , UploadFile
 from langsmith import traceable
 from services.upload_service import handle_upload
+from utils import logger
 
 router = APIRouter()
 
 @router.post("/upload-doc")
 @traceable(name="upload_doc_endpoint")
 async def upload_doc(file: UploadFile = File(...)):
+    try:
+        logger.info(f"Upload request received: filename={file.filename}, content_type={file.content_type}")
 
-    return await handle_upload(file)
+        result = await handle_upload(file)
+
+        logger.info(f"Upload successful: filename={file.filename}")
+        return result
+
+    except HTTPException as e:
+        logger.warning(f"HTTPException during upload: {e.detail}")
+        raise e
+
+    except Exception as e:
+        logger.exception(f"Unexpected error while uploading file: {file.filename}")
+
+        raise HTTPException(
+            status_code=500,
+            detail="Internal Server Error during file upload"
+        )
