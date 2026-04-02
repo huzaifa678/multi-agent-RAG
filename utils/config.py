8310e86a9df4bcd299e310e92135be34ecd7a8e5
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-04-02 18:23:58 +0500

    added langsmith config

--- utils/config.py
+++ utils/config.py
@@ -10,3 +10,6 @@ class Config:
     ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
     TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+    LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
+    LANGCHAIN_PROJECT = os.getenv("LANGCHAIN_PROJECT")
+    LANGCHAIN_TRACING_V2 = os.getenv("LANGCHAIN_TRACING_V2")
